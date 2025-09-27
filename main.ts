5ba7cafc3310e040f4a3de89142de7203ce6f2cb
EpolPers <epol.pers@gmail.com>
2025-09-27 18:05:32 +0700

    Master
    v. 0.1.0

--- main.ts
+++ main.ts
@@ -1,52 +1,66 @@
+/**
+ * При написании кода используются следующие термины для наименования сущностей
+ * Catalog - набор эелементов, которые создаются одномоментно.
+ * Item - единичная структура каталога, которая создается на основании правил предопределенных пользователем.
+ */
+
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { start } from 'repl';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface PluginSettings {
 	mySetting: string;
+	createdElementType: string;
+	valueInput: string;
+	catalogElements: any;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	mySetting: 'default',
+	createdElementType: '',
+	valueInput: '',
+	catalogElements: [],
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Autocreate extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
+		/*const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
-		});
+		});*/
+
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		//ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		/*const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Status Bar Text');*/
 
 		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
+		/*this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
 				new SampleModal(this.app).open();
 			}
-		});
+		});*/
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
+		/*this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
 			editorCallback: (editor: Editor, _view: MarkdownView) => {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
-		});
+		});*/
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
+		/*this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
@@ -63,15 +77,15 @@ export default class MyPlugin extends Plugin {
 					return true;
 				}
 			}
-		});
+		});*/
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new MainSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			//console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -91,7 +105,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+/*class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -105,30 +119,324 @@ class SampleModal extends Modal {
 		const {contentEl} = this;
 		contentEl.empty();
 	}
-}
+}*/
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+class MainSettingTab extends PluginSettingTab {
+	plugin: Autocreate;
+	
+	constructor(app: App, plugin: Autocreate) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
+		const settings = this.plugin.settings;
+		const catalog = this.plugin.settings.catalogElements;
+		const plugin = this.plugin;
 
+		const cssClassContainerElement = 'catalog-item';
+
+		let domCatalog: any[] = [];
+				
 		containerEl.empty();
 
-		new Setting(containerEl)
+		const containerCatalog = containerEl.createEl('div', {
+			cls: 'container-catalog'
+		})
+	
+
+		const catalogSettings = new Setting(containerCatalog)
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.addText(elementNameInput => elementNameInput
+				.setPlaceholder('Name file or folder')
+				//.setValue(this.plugin.settings.catalog)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					settings.valueInput = value;
+				})
+				.setValue(settings.valueInput)
+			)
+			.addButton(button => button
+				.setButtonText("Add file")
+				.setTooltip('Add file')
+				.setIcon("file-plus")
+				.onClick(async () => {
+					if (settings.valueInput.length > 0) {
+						catalog.push({
+						itemType: 'file',
+						itemName: settings.valueInput,
+						itemChilds: [],
+						});
+					uploadCatalogContainer(catalog.length - 1, catalog, containerCatalog);
+					
 					await this.plugin.saveSettings();
-				}));
+					}
+				})
+			)
+			.addButton(button => button
+				.setButtonText("Add folder")
+				.setTooltip('Add folder')
+				.setIcon("folder-plus")
+				.onClick(async () => {
+					
+					if (settings.valueInput.length > 0) {
+						catalog.push({
+						itemType: 'folder',
+						itemName: settings.valueInput,
+						itemChilds: [],
+					});
+					uploadCatalogContainer(catalog.length - 1, catalog, containerCatalog);
+
+			
+					
+					await this.plugin.saveSettings();
+					}
+				})
+			);
+			
+
+
+
+
+			/**
+			 * Обновляет список DOM элементов на основе глобального объекта настроек
+			 * @param indexCreateElement {number | undefinde} индекс элемента, который нужно добавить, если undefined, то обновляет весь список
+			 */
+
+			function uploadCatalogContainer (indexCreateElement: number | undefined, catalogDir: { itemType: string; itemName: string; }[], parentContainer: HTMLElement, nestingLevel = -1) {
+				
+				
+				function deletesNullValue (currentCatalog: any[]){
+					currentCatalog.forEach((value, i) => {
+						if (value == null) {
+							currentCatalog.splice(i, 1)
+						} else if (value.itemChilds.length > 0) {
+							deletesNullValue(value.itemChilds);
+						}
+
+						
+				})
+				}
+
+				deletesNullValue(catalog);
+
+
+				nestingLevel = nestingLevel + 1;
+
+				catalogDir.forEach((value: {
+					itemChilds: any; itemType: string; itemName: string; }, i: number) => {
+					
+						if (value !== null){
+					
+						if (i == indexCreateElement || indexCreateElement == undefined) {
+						
+
+							function createContainer (parentContainer: HTMLElement) {
+											
+							// Создается родительский контейнер элемента
+							let itemContainerEl = parentContainer.createEl('div', {
+								cls: 'setting-item, ' + cssClassContainerElement  
+							});
+							// Добавляем DOM элемент для value
+							//value.domContainer = containerSettingItem
+
+							// Создается контейнер блока информации в родительском контейнере
+							let containerItemInfoEl = itemContainerEl.createEl('div', {
+								cls: 'setting-item-info',
+							})
+							// Создаем информацию в контейнере блока информации
+							let containerItemTypeEl = containerItemInfoEl.createEl('div', {
+								text: '{' + value.itemType + '} ',
+							});
+							let containerItemNameEl = containerItemInfoEl.createEl('p',{
+								text: value.itemName,
+								cls: 'ac-item-name'
+							});
+
+							// Обработчик кнопки Button Rename Item
+							containerItemNameEl.addEventListener('click', () => {
+								containerItemNameEl.classList.add('ac-hold');
+								let inputNewNameItemEl = containerItemInfoEl.createEl('input', {
+									placeholder: 'New name'
+								});
+								let buttonSubmitRenameItemEl = containerItemInfoEl.createEl('button', {
+									text: 'Ok',
+								});
+								inputNewNameItemEl.focus();
+
+								getInputValue(buttonSubmitRenameItemEl, inputNewNameItemEl, value.itemName).then(valueInput => {
+									
+									if (valueInput.length > 0) {
+										value.itemName = valueInput;
+									}
+									
+									containerItemNameEl.classList.remove('ac-hold');
+
+									inputNewNameItemEl.remove();
+									buttonSubmitRenameItemEl.remove();
+									
+									plugin.saveSettings();
+
+									deleteCatalogContainer(containerCatalog);
+									uploadCatalogContainer(undefined, catalog, containerCatalog);
+								});	
+
+							}, { once: true });
+
+
+							
+							// Создается контейнер блока управления в родительском контейнере 
+							let itemControlContainerEl = itemContainerEl.createEl('div', {
+								cls: 'setting-item-control',
+							});
+
+							// Создаем органы управления в контейнере блока управления в зависимости от условий
+							// Если это папка то создается кнопка добавления файлов как орган управления
+							if (value.itemType == 'folder') {
+
+								let buttonAddChildFile = itemControlContainerEl.createEl('button', {
+									text: 'Add File'
+								});
+								let buttonAddChildFolder = itemControlContainerEl.createEl('button', {
+									text: 'Add Folder'
+								});
+								
+								// Обработка кнопки Add Child Folder
+
+								buttonAddChildFolder.addEventListener('click', () => {
+									createItem(itemContainerEl).then(answer => {
+										
+										value.itemChilds.push({
+											itemType: 'folder',
+											itemName: answer,
+											itemChilds: [] 
+										});
+										plugin.saveSettings();
+
+										deleteCatalogContainer(containerCatalog);
+										uploadCatalogContainer(undefined, catalog, containerCatalog);
+									
+									});
+								}, { once: true });
+
+
+								// Обработка кнопки Add Child File
+								buttonAddChildFile.addEventListener('click', () => {
+									createItem(itemContainerEl).then(answer => {
+									
+										value.itemChilds.push({
+											itemType: 'file',
+											itemName: answer,
+											itemChilds: [] 
+										});
+										plugin.saveSettings();
+
+										deleteCatalogContainer(containerCatalog);
+										uploadCatalogContainer(undefined, catalog, containerCatalog);
+									
+									});
+								}, { once: true });
+							
+							}
+							
+							// Создается кнопка удаления данного элемента в контейнер блока управления
+							let buttonElementRemove = itemControlContainerEl.createEl('button', {
+								text: 'Delete'
+							});
+							
+							
+							// Оброботчик кнопки Delete у элемента
+							buttonElementRemove.onclick = function() {	
+								delete catalogDir[i];
+								itemContainerEl.remove();
+								plugin.saveSettings();
+							};
+							
+							if (value.itemChilds.length > 0) {
+
+								uploadCatalogContainer(undefined, value.itemChilds, itemContainerEl, nestingLevel)
+							};
+
+							//uploadDomArray(domCatalog, nestingLevel)
+
+							}
+							createContainer(parentContainer);
+					}
+				}
+				});
+			
+			}
+			/**
+			 * 
+			 * @param deletableCatalogContainerEl: HTMLDivElement - DOM элемент содержащий все ноды каталога  
+			 */
+			function deleteCatalogContainer (deletableCatalogContainerEl: HTMLDivElement){
+				console.log('Вызвана функция удаления контейнера каталога');
+				deletableCatalogContainerEl.querySelectorAll('.' + cssClassContainerElement).forEach((item)=>{
+					item.remove();
+				})
+			}
+
+
+			/**
+			 * 
+			 * @param buttonSubmitEl 
+			 * @param inputEl 
+			 * @returns 
+			 */
+
+			async function getInputValue (buttonSubmitEl: HTMLButtonElement, inputEl: HTMLInputElement, variable: any) {
+				let inputValue = '';
+				let resolvePromise: (value: unknown) => void; // Хранит ссылку на функцию resolve
+				const promise = new Promise((resolve) => {
+					resolvePromise = resolve;
+				});
+				buttonSubmitEl.onclick = () => {
+					inputValue = inputEl.value;
+					resolvePromise(inputValue); // Разрешаем промис при клике
+				};
+
+				variable = promise;
+				return await variable;
+			}
+
+			async function createItem(parentItemContainerEl: HTMLDivElement) {
+				let inputGhostItemValue = '';
+				let resolvePromise: (value: unknown) => void; // Хранит ссылку на функцию resolve
+
+				// Создаем промис, который будет ожидать ввода
+				const promise = new Promise((resolve) => {
+					resolvePromise = resolve;
+				});
+
+				let containerGhostItemEl = parentItemContainerEl.createEl('div', {
+					cls: 'ghost-item-container'
+				});
+
+				let inputGhostItemEl = containerGhostItemEl.createEl('input', {
+					type: 'text'
+				});
+				inputGhostItemEl.focus();
+
+
+				let buttonGhostItemEl = containerGhostItemEl.createEl('button', {
+					text: 'Submit'
+				});
+
+				buttonGhostItemEl.onclick = () => {
+					inputGhostItemValue = inputGhostItemEl.value;
+					resolvePromise(inputGhostItemValue); // Разрешаем промис при клике
+				};
+
+				return await promise;
+		}
+
+			uploadCatalogContainer(undefined, catalog, containerCatalog);
+
+
+			
 	}
+
+	
 }
